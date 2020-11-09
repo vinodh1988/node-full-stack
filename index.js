c3d76247c6ad22c93f8cf755091157c963eab93a
vinodh1988 <k.c.vinodh@hotmail.com>
2020-11-09 10:21:22 +0530

    module exports

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
+const { SCHED_NONE } = require('cluster')
 const fs = require('fs')
- 
+//const obj = require ('./second')
+const {citizen,fun,number_array} = require('./second')
 // Node is asynchronous and nonblocking and it is single threaded platform
+
+// node js follows a module structuring for structuring code
+// internally it uses a module called common js
+
 fs.readFile('package.json','utf8',function(err,data) {
     if(err)
       console.log("Not read -error")
@@ -9,3 +15,6 @@ fs.readFile('package.json','utf8',function(err,data) {
 })
 
 console.log("function aftwards");
+console.log(citizen)
+console.log(number_array)
+fun()
---
+++ second.js
@@ -0,0 +1,17 @@
+const a = [35,35,6,345,36,46,4445]
+
+const person = {
+    sno: 1,
+    name: 'Vijay Kumar',
+    city:'Mumbai'
+}
+
+function temp(){
+  console.log("Temporary function")
+}
+
+module.exports = {
+    number_array : a,
+    citizen : person,
+    fun: temp
+} 
