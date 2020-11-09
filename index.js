ed95d5b73f5643b54ef5557ad8a3756552efe965
vinodh1988 <k.c.vinodh@hotmail.com>
2020-11-09 11:48:19 +0530

    file-response-html

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 //beginning point of the web application
  const express = require("express");
  const app = express();
+ const path = require("path")
 
  app.get("/",function(request,response){
      console.log(request.headers);
      console.log(request.body)
-     response.send("NODE IS UP AND RUNNING")
+    // response.send("NODE IS UP AND RUNNING")
+    response.sendFile(path.join(__dirname,"public/index.html"))
  })
 
- app.get
+
  app.listen("9999",function(){
      console.log("server is started @", 9999)
      
