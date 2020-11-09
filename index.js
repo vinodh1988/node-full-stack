38dd41f1201bd7501554a9c36c9202a821bf4418
vinodh1988 <k.c.vinodh@hotmail.com>
2020-11-09 11:33:20 +0530

    first-request-commit

--- index.js
+++ index.js
@@ -2,7 +2,14 @@
  const express = require("express");
  const app = express();
 
+ app.get("/",function(request,response){
+     console.log(request.headers);
+     console.log(request.body)
+     response.send("NODE IS UP AND RUNNING")
+ })
 
+ app.get
  app.listen("9999",function(){
      console.log("server is started @", 9999)
+     
  })
